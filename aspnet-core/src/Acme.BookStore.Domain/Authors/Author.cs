3695e4e3b487d4a219b2de1cf3c52fdf79bafaa1
MohamedMBadr <MohamedMBadr97@gmail.com>
2024-01-04 01:01:31 +0200

    initial commit

---
+++ aspnet-core/src/Acme.BookStore.Domain/Authors/Author.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.BookStore.Authors
+{
+    public class Author : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; private set; }
+        public DateTime BirthDate { get; set; }
+        public string ShortBio { get; set; }
+
+
+        private Author() { }
+
+        internal Author(Guid id, string name, DateTime birthDate, string? shortBio) : base(id)
+        {
+            setName(name);
+            BirthDate = birthDate;
+            ShortBio = shortBio;
+
+        }
+        private void setName(string name)
+        {
+            Name = Check.NotNullOrWhiteSpace(
+                name, nameof(name),
+                maxLength: AuthorConsts.MaxNameLength
+                );
+        }
+        internal Author ChangeName(string name)
+        {
+            setName(name);
+            return this;
+        }
+    }
+}
---
+++ aspnet-core/src/Acme.BookStore.Domain/Authors/AuthorManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace Acme.BookStore.Authors
+{
+    public class AuthorManager :DomainService
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorManager(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<Author> CreateAsync(string name, DateTime birthDate, string? shortBoi = null)
+        {
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+            var existingAuthor = await _authorRepository.FindByNameAsync(name);
+
+            if (existingAuthor != null)
+            {
+                throw new AuthorAlreadyExistsException(name);
+            }
+            return new Author(
+                GuidGenerator.Create(),
+                name,
+                birthDate,
+                shortBoi
+                );
+        }
+        public async Task ChangeNameAsync(Author author , string newName)
+        {
+            Check.NotNull(newName, nameof(newName));
+            Check.NotNull(author , nameof(author));
+
+            var existingAuthor = await _authorRepository.FindByNameAsync(newName);
+            if (existingAuthor != null && existingAuthor.Id != author.Id)
+            {
+                throw new AuthorAlreadyExistsException(newName);
+
+            }
+            author.ChangeName(newName);
+        }
+    }
+}
